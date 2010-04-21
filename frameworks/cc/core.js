872fa7c8c5c401da9b5a2c4e7b86d9822353f78d
Aaron Unger <aunger@concord.org>
2010-04-21 11:45:41 -0400

    Add frameworks initial implementation, since this is intended to be used as a library.

---
+++ frameworks/cc/core.js
@@ -0,0 +1,21 @@
+// ==========================================================================
+// Project:   CC
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals CC */
+
+/** @namespace
+
+  My cool new framework.  Describe your framework.
+  
+  @extends SC.Object
+*/
+CC = SC.Object.create(
+  /** @scope CC.prototype */ {
+
+  NAMESPACE: 'CC',
+  VERSION: '0.1.0'
+
+  // TODO: Add global constants or singleton objects needed by your app here.
+
+}) ;
