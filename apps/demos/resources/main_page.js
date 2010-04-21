576af761db799d70440d0779923620fe8cb4c5f2
sfentress <sfentress@concord.org>
2010-04-21 15:44:28 -0400

    Add tabbed view for main demo page. Add sample "inner pages" tabbed view

--- apps/demos/resources/main_page.js
+++ apps/demos/resources/main_page.js
@@ -11,12 +11,54 @@ Demos.mainPage = SC.Page.design({
   // Add childViews to this pane for views to display immediately on page 
   // load.
   mainPane: SC.MainPane.design({
-    childViews: 'mwAppletView'.w(),
+    childViews: 'tabView'.w(),
     
-    mwAppletView: CC.MwAppletView.design({
-      cmlUrl: "http://mw2.concord.org/public/student/classic/machine/bike.cml",
-      layout: { centerX: 0, centerY: 0, width: 500, height: 400 }
+    tabView: SC.TabView.design({ 
+      layout: {top: 30, bottom: 5, left: 5, right: 5 }, 
+      items: [ 
+        {title: "Welcome", value: "demos" },
+        {title: "Applet", value: "mwAppletView" },
+        {title: "Inner pages", value: "innerTabView" } 
+      ], 
+      itemTitleKey: 'title', 
+      itemValueKey: 'value', 
+      nowShowing: 'demos' // defining the startup tab 
     })
+    
+  }),
+  
+  // sample MW applet view
+  mwAppletView: CC.MwAppletView.design({
+    cmlUrl: "http://mw2.concord.org/public/student/classic/machine/bike.cml",
+    layout: { centerX: 0, centerY: 0, width: 500, height: 400 }
+  }),
+  
+  // sample view of inner pages
+  innerTabView: SC.TabView.design({
+    layout: {centerX: 0, centerY: 0, width: 500, height: 400  }, 
+    items: [ 
+      {title: "Page 1", value: "page1" },
+      {title: "Page 2", value: "page2" } 
+    ], 
+    itemTitleKey: 'title', 
+    itemValueKey: 'value', 
+    nowShowing: 'page1' // defining the startup tab
+  }),
+  
+  demos: SC.LabelView.design({
+    escapeHTML: NO,
+    value: "<h1>Welcome</h1><p>CC SproutCore component demos</p>",
+    layout: { centerX: 0, centerY: 0, width: 500, height: 400 }
+  }),
+  
+  page1: SC.LabelView.design({
+    escapeHTML: NO,
+    value: "<h1>Page 1</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>"
+  }),
+  
+  page2: SC.LabelView.design({
+    escapeHTML: NO,
+    value: "<h1>Page 2</h1><p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
   })
 
 });
