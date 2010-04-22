23c6113ea5defb2d0400d6b13d2014947afbb930
Aaron Unger <aunger@concord.org>
2010-04-22 14:30:25 -0400

    Refactor the QuestionView into a simple open-response question.
    Note: It doesn't use any models or controllers right now.

--- frameworks/cc/views/question.js
+++ frameworks/cc/views/question.js
@@ -10,22 +10,48 @@
 
   @extends SC.View
 */
-CC.QuestionView = SC.View.extend(SC.ContentDisplay, {
+CC.QuestionView = SC.View.extend({
+	
+	layout: {top: 0, left: 0, right: 0, bottom: 0},
 
   classNames: ['question'],
 
-  contentDisplayProperties: 'prompt input'.w(),
+  contentDisplayProperties: 'prompt'.w(),
 
-  render: function(context, firstTime) {
-    var prompt = '[prompt]';
-    var input = '[input]';
-    
-    var content = this.get('content');
-    if (content != null) {
-      prompt = content.get('prompt');
-      input = content.get('input');
-    }
-      
-    context.push("Prompt: "+prompt+", Input: "+input);
-  }
+	prompt: "[prompt]",
+
+  // childViews: 'promptView inputView'.w(),
+	childViews: 'stackView'.w(),
+	
+	stackView: SC.StackedView.design({
+		layout: {top: 0, left: 0, right: 0},
+		
+		childViews: 'promptView inputView'.w(),
+		promptBinding: "*parentView.prompt",
+		
+		promptView: SC.LabelView.design({
+			classNames: 'question-prompt',
+			useStaticLayout: YES,
+			escapeHTML: NO,
+			layout: {top: 15, left: 5, right: 5},
+			valueBinding: "*parentView.prompt",
+			render: function(context, firstTime) {
+				sc_super();
+				context.addStyle('height', 'auto');
+				context.addStyle('bottom', null);
+			}
+		}),
+
+		inputView: SC.TextFieldView.design({
+			classNames: 'question-input',
+			useStaticLayout: YES,
+			isTextArea: YES,
+			layout: {left: 15, width: 400, top: 15, height: 95 },
+			render: function(context, firstTime) {
+				sc_super();
+				// context.addStyle('height', null);
+			}
+		})
+	})
+
 });
