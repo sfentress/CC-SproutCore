0c65d6bb9bef977a2278e3a7191193cc7428e6ba
sfentress <sfentress@concord.org>
2010-04-26 17:15:27 -0400

    Basic tests for MW Applet and Multiple choices.

--- frameworks/cc/tests/views/multiple_choice_question.js
+++ frameworks/cc/tests/views/multiple_choice_question.js
@@ -2,14 +2,45 @@
 // Project:   Cc.MultipleChoiceQuestionView Unit Test
 // Copyright: ©2010 My Company, Inc.
 // ==========================================================================
-/*globals Cc module test ok equals same stop start */
+/*globals CC module test ok equals same stop start */
 
-module("Cc.MultipleChoiceQuestionView");
+var mwChoiceView, pane, mwChoiceViewRendered;
+
+var CHOICES = "one two three four five".w();
+
+module("CC.MultipleChoiceQuestionView", {
+  setup: function() {
+    SC.RunLoop.begin();
+    mwChoiceView = CC.MultipleChoiceQuestionView.design({
+      prompt: "What do you think of this different fine question?<br/>This <span style='font-weight: bold;'>question</span> has <span style='font-style: oblique;'>styling</span>.",
+  		choices: CHOICES
+    });
+    
+    pane = SC.MainPane.create({
+      childViews: [mwChoiceView]
+    });
+    pane.append();
+    SC.RunLoop.end();
+  
+    mwChoiceViewRendered = pane.childViews[0];
+  },
+  
+  teardown: function() {
+    pane.remove();
+    pane = mwChoiceView = mwChoiceViewRendered = null;
+  }
+});
 
 // TODO: Replace with real unit test for Cc.MultipleChoiceQuestionView
-test("test description", function() {
-  var expected = "test";
-  var result   = "test";
-  equals(result, expected, "test should equal test");
+test("multiple choice question should contain choice radio buttons", function() {
+  var input = mwChoiceViewRendered.$('.question-input');
+  
+  ok(input.find('input').size() === 5, "There should be five choices");
+  
+  var firstChoice = input.find('.sc-button-label').first().html();
+  equals(firstChoice, "one", "first choice should be 'one'");
+  
+  var lastChoice = input.find('.sc-button-label').last().html();
+  equals(lastChoice, "five", "last choice should be 'five'");
 });
 
--- frameworks/cc/tests/views/mw_applet.js
+++ frameworks/cc/tests/views/mw_applet.js
@@ -2,14 +2,48 @@
 // Project:   Cc.MwAppletView Unit Test
 // Copyright: ©2010 My Company, Inc.
 // ==========================================================================
-/*globals Cc module test ok equals same stop start */
+/*globals CC module test ok equals same stop start */
 
-module("Cc.MwAppletView");
+var mwAppletView, pane, mwAppletViewRendered;
 
-// TODO: Replace with real unit test for Cc.MwAppletView
-test("test description", function() {
-  var expected = "test";
-  var result   = "test";
-  equals(result, expected, "test should equal test");
+var CML_URL = "http://mw2.concord.org/public/student/classic/machine/bike.cml";
+
+module("CC.MwAppletView", {
+  setup: function() {
+    SC.RunLoop.begin();
+    mwAppletView = CC.MwAppletView.design({
+      cmlUrl: CML_URL
+    });
+    
+    pane = SC.MainPane.create({
+      childViews: [mwAppletView]
+    });
+    pane.append();
+    SC.RunLoop.end();
+  
+    mwAppletViewRendered = pane.childViews[0];
+  },
+  
+  teardown: function() {
+    pane.remove();
+    pane = mwAppletView = mwAppletViewRendered = null;
+  }
+});
+
+test("applet should contain MW jar and CML file", function() {
+  
+  // we should be able to do this:
+  //    var applet   = mwAppletViewRendered.$('applet');
+  //    var archive  = applet.attr('archive')
+  //    [check that archive is correct]
+  // but applet.attr('archive') throws an error (while applet.html() works just fine)
+  
+  var appletHTML   = mwAppletViewRendered.$().html();
+  ok(appletHTML.indexOf('http://mw2.concord.org/public/lib/mwapplet.jar') > -1, 'applet contains MW jar');
+  
+  
+  var applet   = mwAppletViewRendered.$('applet');
+  var param = applet.html();
+  ok(param.indexOf(CML_URL) > -1, 'applet contains param with cml file');
 });
 
