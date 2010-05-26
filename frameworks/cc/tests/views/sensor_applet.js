2ecc582db0d9694d0592b64f49774cd499f62e1a
Aaron Unger <aunger@concord.org>
2010-05-26 12:18:13 -0400

    Add sensor applet, with demo which outputs datapoints into a ListView.

---
+++ frameworks/cc/tests/views/sensor_applet.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   Cc.SensorAppletView Unit Test
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals Cc module test ok equals same stop start */
+
+module("Cc.SensorAppletView");
+
+// TODO: Replace with real unit test for Cc.SensorAppletView
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
