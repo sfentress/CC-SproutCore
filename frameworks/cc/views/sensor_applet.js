93958b9b7157963c3599f82ac519fd54c351166e
Aaron Unger <aunger@concord.org>
2010-06-01 11:58:36 -0400

    Rearrange sensor applet properties to make it clearer what needs to be overridden. Add safari detection.

--- frameworks/cc/views/sensor_applet.js
+++ frameworks/cc/views/sensor_applet.js
@@ -13,14 +13,43 @@
 CC.SensorAppletView = CC.AppletView.extend(
 /** @scope CC.SensorAppletView.prototype */ {
 	
-	// TODO This only supports a Vernier GoMotion right now...
-	resourcePath: '/simple.otml',
-	
+	//OVERRIDES!!! These are typically overridden in the page definitions
 	// This is the Javascript object path to the listener function... eg App.mainPage.mainPane.sensorApplet.sensorListener
 	listenerPath: 'defaultDataListener',
 	
+	// the Javascript object path to the sensorState variable, which needs to be used under safari to trigger starting and stopping the sensors
+	safariSensorStatePath: null,
+	
+	// these need to be overridden when implemented, if you want to use this view as the data listener
+	// called whenever data is received in the sensor. dataPoints is an array of floats
+	dataRecieved: function(dataType, numberOfDataPoints, dataPoints) {},
+	
+	// called whenever meta data about the data stream changes
+	dataStreamEvent: function(dataType, numberOfDataPoints, dataPoints) {},
+	
+	// called when the applet is done initializing itself, and the sensors are ready to be used
+	sensorsReady: function() {},
+	
+	// END OVERRIDES!!!
+	
+	// TODO This only supports a Vernier GoMotion right now...
+	resourcePath: '/simple.otml',
+	
+	isSafari: function() {
+		// detect safari
+		if (navigator.vendor !== null && navigator.vendor.indexOf("Apple") != -1) {
+			return YES;
+		}
+		return NO;
+	}(),
+	
 	// this is the javascript object patch to a String variable which will store the current sensor applet's state: "ready", "running", "stopped"
-	sensorStatePath: null,
+	sensorStatePath: function() {
+		if (this.get('isSafari')) {
+			return this.get('safariSensorStatePath');
+		}
+		return null;
+	}.property('isSafari', 'safariSensorStatePath'),
 	// the current sensor applet's state: "ready", "running", "stopped". If sensorStatePath is set, the applet will watch this variable for changes to trigger
 	// starting and stopping the sensors. This is necessary on Safari on Mac OSX since the javascript can sometimes not call applet methods directly.
 	sensorState: "ready",
@@ -60,16 +89,6 @@ CC.SensorAppletView = CC.AppletView.extend(
 	
 	classNames: "sensor-applet",
 	
-	layout: { centerX: 0, centerY: 0, width: 160, height: 40 },     // defaults
-	
-	// these need to be overridden when implemented, if you want to use this view as the data listener
-	// called whenever data is received in the sensor. dataPoints is an array of floats
-	dataRecieved: function(dataType, numberOfDataPoints, dataPoints) {},
-	
-	// called whenever meta data about the data stream changes
-	dataStreamEvent: function(dataType, numberOfDataPoints, dataPoints) {},
-	
-	// called when the applet is done initializing itself, and the sensors are ready to be used
-	sensorsReady: function() {}
+	layout: { centerX: 0, centerY: 0, width: 160, height: 40 }     // defaults
 
 });
